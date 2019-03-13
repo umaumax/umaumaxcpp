257bc594a7bd9369124d95e3692d8bf6a4e2cd83
umaumax <umauma7083@gmail.com>
2019-03-13 20:54:17 +0900

    Add inline to picojson_util.hpp

--- picojson_util.hpp
+++ picojson_util.hpp
@@ -7,27 +7,27 @@
 
 template <class T>
 struct PicoJsonUtil {
-  static T Decode(picojson::value& value);
-  static picojson::value Encode(T& value);
+  static inline T Decode(picojson::value& value);
+  static inline picojson::value Encode(T& value);
 };
 template <>
-int PicoJsonUtil<int>::Decode(picojson::value& value) {
+inline int PicoJsonUtil<int>::Decode(picojson::value& value) {
   return static_cast<int>(value.get<double>());
 }
 template <>
-float PicoJsonUtil<float>::Decode(picojson::value& value) {
+inline float PicoJsonUtil<float>::Decode(picojson::value& value) {
   return static_cast<float>(value.get<double>());
 }
 template <>
-double PicoJsonUtil<double>::Decode(picojson::value& value) {
+inline double PicoJsonUtil<double>::Decode(picojson::value& value) {
   return value.get<double>();
 }
 template <>
-std::string PicoJsonUtil<std::string>::Decode(picojson::value& value) {
+inline std::string PicoJsonUtil<std::string>::Decode(picojson::value& value) {
   return value.get<std::string>();
 }
 template <class T>
-T PicoJsonUtil<T>::Decode(picojson::value& value) {
+inline T PicoJsonUtil<T>::Decode(picojson::value& value) {
   using E = typename T::value_type;
   std::vector<E> ret;
   for (auto&& pico_elem : value.get<picojson::array>()) {
@@ -37,23 +37,23 @@ T PicoJsonUtil<T>::Decode(picojson::value& value) {
 }
 
 template <>
-picojson::value PicoJsonUtil<int>::Encode(int& value) {
+inline picojson::value PicoJsonUtil<int>::Encode(int& value) {
   return picojson::value(static_cast<double>(value));
 }
 template <>
-picojson::value PicoJsonUtil<float>::Encode(float& value) {
+inline picojson::value PicoJsonUtil<float>::Encode(float& value) {
   return picojson::value(static_cast<double>(value));
 }
 template <>
-picojson::value PicoJsonUtil<double>::Encode(double& value) {
+inline picojson::value PicoJsonUtil<double>::Encode(double& value) {
   return picojson::value(value);
 }
 template <>
-picojson::value PicoJsonUtil<std::string>::Encode(std::string& value) {
+inline picojson::value PicoJsonUtil<std::string>::Encode(std::string& value) {
   return picojson::value(value);
 }
 template <class T>
-picojson::value PicoJsonUtil<T>::Encode(T& values) {
+inline picojson::value PicoJsonUtil<T>::Encode(T& values) {
   using E = typename T::value_type;
   picojson::array pico_array;
   for (auto&& e : values) {
