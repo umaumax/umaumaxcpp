2e043a2b54ba3f979e576dd1d7f671af6480c9d2
umaumax <umauma7083@gmail.com>
2019-03-31 14:45:42 +0900

    Refactor

--- cmake/common/ExternalProjectAdd_GTest.cmake
+++ cmake/common/ExternalProjectAdd_GTest.cmake
@@ -1,8 +1,9 @@
 # FYI: [CMake ExternalProject\_Add for Google Mock \(gmock\) and Google Test \(gtest\) Libraries]( https://gist.github.com/ClintLiddick/51deffb768a7319e715071aa7bd3a3ab )
 find_package(Threads REQUIRED)
 set(GTEST_FOUND TRUE)
+set(${GTEST_TARGET} ${GTEST_TARGET})
 include(ExternalProject)
-ExternalProject_Add(googletest
+ExternalProject_Add(${GTEST_TARGET}
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG        mster
   UPDATE_COMMAND ""
@@ -12,11 +13,11 @@ ExternalProject_Add(googletest
   LOG_BUILD ON
   )
 
-ExternalProject_Get_Property(googletest source_dir)
+ExternalProject_Get_Property(${GTEST_TARGET} source_dir)
 set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include)
 set(GMOCK_INCLUDE_DIRS ${source_dir}/googlemock/include)
 
-ExternalProject_Get_Property(googletest binary_dir)
+ExternalProject_Get_Property(${GTEST_TARGET} binary_dir)
 set(GTEST_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
 set(GTEST_LIBRARY gtest)
 add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
@@ -24,7 +25,7 @@ set_target_properties(${GTEST_LIBRARY} PROPERTIES
   IMPORTED_LOCATION ${GTEST_LIBRARY_PATH}
   # NOTE: ${CMAKE_THREAD_LIBS_INIT} may be blank
   IMPORTED_LINK_INTERFACE_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}")
-add_dependencies(${GTEST_LIBRARY} googletest)
+add_dependencies(${GTEST_LIBRARY} ${GTEST_TARGET})
 
 set(GTEST_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a)
 set(GTEST_MAIN_LIBRARY gtest_main)
@@ -32,7 +33,7 @@ add_library(${GTEST_MAIN_LIBRARY} UNKNOWN IMPORTED)
 set_target_properties(${GTEST_MAIN_LIBRARY} PROPERTIES
   IMPORTED_LOCATION ${GTEST_MAIN_LIBRARY_PATH}
   IMPORTED_LINK_INTERFACE_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}")
-add_dependencies(${GTEST_MAIN_LIBRARY} googletest)
+add_dependencies(${GTEST_MAIN_LIBRARY} ${GTEST_TARGET})
 
 set(GMOCK_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock.a)
 set(GMOCK_LIBRARY gmock)
@@ -40,7 +41,7 @@ add_library(${GMOCK_LIBRARY} UNKNOWN IMPORTED)
 set_target_properties(${GMOCK_LIBRARY} PROPERTIES
   IMPORTED_LOCATION ${GMOCK_LIBRARY_PATH}
   IMPORTED_LINK_INTERFACE_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}")
-add_dependencies(${GMOCK_LIBRARY} googletest)
+add_dependencies(${GMOCK_LIBRARY} ${GTEST_TARGET})
 
 set(GMOCK_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock_main.a)
 set(GMOCK_MAIN_LIBRARY gmock_main)
--- cmake/common/ExternalProjectAdd_PicoJSON.cmake
+++ cmake/common/ExternalProjectAdd_PicoJSON.cmake
@@ -1,5 +1,6 @@
 include(ExternalProject)
-ExternalProject_Add(picojson
+set(PICOJSON_TARGET picojson)
+ExternalProject_Add(${PICOJSON_TARGET}
   GIT_REPOSITORY  https://github.com/kazuho/picojson.git
   GIT_TAG         master
   CONFIGURE_COMMAND ""
@@ -10,28 +11,7 @@ ExternalProject_Add(picojson
   LOG_DOWNLOAD ON
   )
 
-# include(ExternalProject)
-# ExternalProject_Add(cxxopts
-# GIT_REPOSITORY    https://github.com/jarro2783/cxxopts.git
-# GIT_TAG           master
-# SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/cxxopts-build"
-# CONFIGURE_COMMAND ""
-# BUILD_COMMAND     ""
-# INSTALL_COMMAND   ""
-# TEST_COMMAND      ""
-# BUILD_IN_SOURCE TRUE
-# )
-# set(cxxopts_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/cxxopts-build/include)
-# target_include_directories(${PROJECT_NAME} PRIVATE
-# ${cxxopts_INCLUDE_DIR}
-# )
-# add_dependencies(${PROJECT_NAME} cxxopts)
-
-set(PicoJSON_FOUND TRUE)
-ExternalProject_Get_Property(picojson source_dir)
-set(PicoJSON_INCLUDE_DIRS ${source_dir})
-set(PicoJSON_LIBRARIES)
-
 set(PICOJSON_FOUND TRUE)
+ExternalProject_Get_Property(${PICOJSON_TARGET} source_dir)
 set(PICOJSON_INCLUDE_DIRS ${source_dir})
 set(PICOJSON_LIBRARIES)
