83343fd58b69a7839f19ed268b43d640ac6b312d
umaumax <umauma7083@gmail.com>
2019-04-11 22:47:58 +0900

    Add std::array and raw array to streampp

--- streampp/streampp.hpp
+++ streampp/streampp.hpp
@@ -16,16 +16,88 @@ std::ostream& operator<<(std::ostream& ostr, const std::vector<T>& v) {
   return ostr;
 }
 
+#include <array>
+template <typename T, std::size_t N>
+std::ostream& operator<<(std::ostream& ostr, const std::array<T, N>& ary) {
+  ostr << "[" << ary.size() << "]:";
+  if (ary.empty()) {
+    ostr << "{ }";
+    return ostr;
+  }
+  ostr << "{" << ary.front();
+  for (auto itr = ary.begin() + 1; itr != ary.end(); itr++)
+    ostr << ", " << *itr;
+  ostr << "}";
+  return ostr;
+}
+
+namespace internal {
+template <typename T, int N>
+std::ostream& cout(std::ostream& ostr, T (&ary)[N]) {
+  ostr << "[" << N << "]:";
+  if (N == 0) {
+    ostr << "{ }";
+    return ostr;
+  }
+  ostr << "{" << ary[0];
+  for (auto itr = ary + 1; itr != ary + N; itr++) ostr << ", " << *itr;
+  ostr << "}";
+  return ostr;
+}
+template <typename T, int N>
+std::ostream& cout(std::ostream& ostr, int8_t (&ary)[N]) {
+  ostr << "[" << N << "]:";
+  if (N == 0) {
+    ostr << "{ }";
+    return ostr;
+  }
+  ostr << "{" << static_cast<int32_t>(ary[0]);
+  for (auto itr = ary + 1; itr != ary + N; itr++)
+    ostr << ", " << static_cast<int32_t>(*itr);
+  ostr << "}";
+  return ostr;
+}
+template <typename T, int N>
+std::ostream& cout(std::ostream& ostr, uint8_t (&ary)[N]) {
+  ostr << "[" << N << "]:";
+  if (N == 0) {
+    ostr << "{ }";
+    return ostr;
+  }
+  ostr << "{" << static_cast<uint32_t>(ary[0]);
+  for (auto itr = ary + 1; itr != ary + N; itr++)
+    ostr << ", " << static_cast<uint32_t>(*itr);
+  ostr << "}";
+  return ostr;
+}
+}  // namespace internal
+
+#define STREAMPP_DEF_RAW_ARRAY(type)                            \
+  template <int N>                                              \
+  std::ostream& operator<<(std::ostream& ostr, type(&ary)[N]) { \
+    return internal::cout<type, N>(ostr, ary);                  \
+  }
+STREAMPP_DEF_RAW_ARRAY(int8_t);
+STREAMPP_DEF_RAW_ARRAY(int16_t);
+STREAMPP_DEF_RAW_ARRAY(int32_t);
+STREAMPP_DEF_RAW_ARRAY(int64_t);
+STREAMPP_DEF_RAW_ARRAY(uint8_t);
+STREAMPP_DEF_RAW_ARRAY(uint16_t);
+STREAMPP_DEF_RAW_ARRAY(uint32_t);
+STREAMPP_DEF_RAW_ARRAY(uint64_t);
+STREAMPP_DEF_RAW_ARRAY(float);
+STREAMPP_DEF_RAW_ARRAY(double);
+
 #include <utility>
 template <typename Key, typename T>
 std::ostream& operator<<(std::ostream& ostr, const std::pair<Key, T>& m) {
-  ostr << "{\"" << m.first << "\": "
-       << m.second << "}";
+  ostr << "{\"" << m.first << "\": " << m.second << "}";
   return ostr;
 }
 #include <unordered_map>
 template <typename Key, typename T>
-std::ostream& operator<<(std::ostream& ostr, const std::unordered_map<Key, T>& m) {
+std::ostream& operator<<(std::ostream& ostr,
+                         const std::unordered_map<Key, T>& m) {
   if (m.empty()) {
     ostr << "{ }";
     return ostr;
--- streampp/test/main.cpp
+++ streampp/test/main.cpp
@@ -25,6 +25,82 @@ TEST(streampp_test, map) {
   EXPECT_EQ(ss.str(), "{{\"one\": 1}, {\"two\": 2}}");
 }
 
+TEST(streampp_test, array) {
+  std::stringstream ss;
+  std::array<int, 4> ary = {{0, 1, 2, 3}};
+  ss << ary;
+  EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+}
+
+TEST(streampp_test, raw_array) {
+  {
+    std::stringstream ss;
+    int ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    int8_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    int16_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    int32_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    int64_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    uint8_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    uint16_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    uint32_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    uint64_t ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    float ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+  {
+    std::stringstream ss;
+    double ary[4] = {0, 1, 2, 3};
+    ss << ary;
+    EXPECT_EQ(ss.str(), "[4]:{0, 1, 2, 3}");
+  }
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
